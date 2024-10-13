812845a29d2d6722575852ea7a3d1e98593164c9
Vivek Sundli <viveksundli@Vivek-ka-MacBook-Pro.local>
2024-10-13 19:36:03 +0530

    initial commit

---
+++ src/main/java/com/luffy/wordc/WordcApplication.java
@@ -0,0 +1,13 @@
+package com.luffy.wordc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luffy/wordc/WordcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luffy.wordc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
